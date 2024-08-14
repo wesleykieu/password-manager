fc569fd958e6a2380ca41ce9760d5d14e08dbcdc
Wesley Kieu <wesleykieu13@gmail.com>
2024-08-13 22:57:47 -0700

    New project to push to GitHub

---
+++ pass-manager.py
@@ -0,0 +1,35 @@
+import hashlib
+import getpass
+
+password_manager = {}
+
+def create_account():
+    username = input("Create your username: ")
+    password = getpass.getpass("Create your password: ")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    password_manager[username] = hashed_password
+    print("Account created successfully")
+
+def login(): 
+    username = input("Enter username: ")
+    password = getpass.getpass("Enter your password: ")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    if username in password_manager.keys() and password_manager[username] == hashed_password:
+        print("Login successful")
+    else:
+        print("Invalid username or password")
+
+def main():
+    while True:
+        choice = input("Enter 1 to create an account, 2 to login or 0 to exit: ")
+        if choice == "1":
+            create_account()
+        elif choice == "2":
+            login()
+        elif choice == "0":
+            break
+        else:
+            print("Invalid Choice")
+
+if __name__ == "__main__":
+    main()
